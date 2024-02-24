90272e980fdd5cd8a905a4df9e2dbd9873479daa
JeimyPinto <contanctojeimypinto@gmail.com>
2024-02-24 00:15:51 -0500

    cambios finales

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+// Route for the homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
